d135f81eb06ddc47819282e916be2faf4580a98d
Kristall01 <14259961+Kristall01@users.noreply.github.com>
2021-10-06 22:08:19 +0200

    fixed a bug where frontend.zip was not copied to jar

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ gradle build
 cd src/main/frontend/src
 zip -q -r $base/build/frontend.zip *
 cd $base/build
-zip -ur ../setserver.jar frontend.zip
 cp libs/setserver.jar ..
+zip -qur ../setserver.jar frontend.zip
